03f7f9d3bbf3c2b30ff8ef8e0a2b07e5fb761dce
tianpf <pengfei.tian@beantechs.com>
2020-09-21 17:16:41 +0800

    迭代器模式

---
+++ javase-tpf/src/com/tpf/designpattern/迭代器模式/Client.java
@@ -0,0 +1,10 @@
+package com.tpf.designpattern.迭代器模式;
+
+/**
+ * <p> 迭代器模式 </p >
+ *
+ * @author : tianpf
+ * @version :  Client.java,v 1.0, 2020/9/21-17:15 tianpengfei Exp $
+ */
+public class Client {
+}
